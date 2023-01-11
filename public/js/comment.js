529a8c8f71fa5271408a84fdfd635abb2f69cbb2
Ryan Taylor <ryan.taylor1@yahoo.com>
2023-01-11 11:09:28 -0800

    added to js

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,25 +1,31 @@
+//submits comment when button is clicked
+
 const commentFormHandler = async (event) => {
-    
     event.preventDefault();
-    
-    const comment = document.querySelector('#comment').value.trim();
-    
-    if (comment) {
+
+    const comment_text = document.querySelector('#comment-text').value.trim();
+    const project_id = document.querySelector('#project-id').value.trim();
+
+
+    if (comment_text && project_id) {
         const response = await fetch(`/api/comments`, {
             method: 'POST',
-            body: JSON.stringify({ comment }),
+            body: JSON.stringify({ comment_text, project_id }),
             headers: {
                 'Content-Type': 'application/json',
             },
         });
-        
+
         if (response.ok) {
-            document.location.replace('/project');
+            document.location.replace('/dashboard');
         } else {
+
             alert('Failed to create comment');
             throw new Error(response.statusText);
         }
     }
+
+
 };
 
 document.querySelector('.comment-btn').addEventListener('submit', commentFormHandler);
